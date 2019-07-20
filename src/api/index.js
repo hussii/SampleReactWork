732986d985add718f3c43dc4f8cb84705d086d5c
kashir_gul@hotmail.com <kashir_gul@hotmail.com>
2019-07-20 20:20:24 +0500

    axios API file

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,58 @@
 import axios from 'axios';
+import config from 'Constants/AppConfig';
 
-export default
-   axios.create({
-      baseURL: 'http://reactify.theironnetwork.org/data/',
-      timeout: 2000
-   });
+var axiosObj = axios.create({
+   // baseURL: 'http://reactify.theironnetwork.org/data/',
+   baseURL: config.apiBaseUrl + '/',
+   timeout: 2000
+});
+
+var token = '';
+var user = localStorage.getItem('user');
+if (user) {
+   const profile = user.profile;
+   if (profile) {
+      token = profile.user.token;
+   }
+}
+
+axiosObj.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+
+var methods = {
+   get: async function (endPoint, data, token) {
+      try {
+         return await axiosObj.get(endPoint, {
+            params: data
+         });
+      } catch (error) {
+         return error;
+      }
+   },
+   put: async function (endPoint, data) {
+      try {
+         return await axiosObj.put(endPoint, data);
+      } catch (error) {
+         return error;
+      }
+
+   },
+   post: async function (endPoint, data) {
+      try {
+         return await axiosObj.post(endPoint, data);
+      } catch (error) {
+         return error;
+      }
+
+   },
+   delete: async function (endPoint, data) {
+      try {
+         return await axiosObj.delete(endPoint, {
+            params: data
+         });
+      } catch (error) {
+         return error;
+      }
+   }
+}
+
+export default methods;
