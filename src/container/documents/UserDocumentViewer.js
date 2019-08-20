a5e13adc52e8c3fce35caa6cc677bf2aa6d6fd34
Muhammad Qasim <kashir_gul@hotmail.com>
2019-08-20 15:00:25 +0500

    initial layout changes

--- src/container/documents/UserDocumentViewer.js
+++ src/container/documents/UserDocumentViewer.js
@@ -2,27 +2,71 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import { getViewingDocument } from "Actions";
+import { makeStyles } from '@material-ui/styles';
+import RctSectionLoader from 'Components/RctSectionLoader/RctSectionLoader';
+
+const useStyles = makeStyles({
+    documentViewerContainer: {
+        height: "calc(100vh - 83px)",
+        background: "white",
+        display: "flex",
+        flexDirection: 'row'
+    },
+    documentActionPanel: {
+        width: '300px'
+    },
+    documentNavPanel: {
+        width: '76px'
+    },
+    documentViewer: {
+        width: "calc(100% - 376px)",
+        backgroundColor: '#F9F9F9'
+    }
+});
+
+
+
+const DocumentViewerLayout = function (props) {
+    console.log('DocumentViewerLayout props:', props);
+    const classes = useStyles();
+
+    return (
+        <div className={classes.documentViewerContainer}>
+            <div className={classes.documentViewer}>
+
+            </div>
+            <div className={classes.documentActionPanel}>
+
+            </div>
+            <div className={classes.documentNavPanel}>
+
+            </div>
+        </div>
+    )
+}
 
 class UserDocumentViewer extends Component {
     constructor(props) {
         super(props);
     }
 
+
     componentDidMount() {
-        this.props.getViewingDocument();
+        // this.props.getViewingDocument();
     }
 
     render() {
+
         const { viewingDocument, loading } = this.props;
         console.log('viewingDocument:', viewingDocument);
-        if (loading) {
-            return (
-                <RctSectionLoader />
-            )
-        }
+        // if (loading) {
+        //     return (
+        //         <RctSectionLoader />
+        //     )
+        // }
 
         return (
-            <h3>User document Viewer</h3>
+            <DocumentViewerLayout {...this.props} />
         )
     }
 }
